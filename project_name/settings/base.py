53c461cdc9293295699cf4c336faa5d8e6bcd2fc
jonathan <jonathan@calazan.com>
2015-04-12 16:24:51 -0500

    Added static files dirs and template dirs options.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = '{{ project_name }}.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,6 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
